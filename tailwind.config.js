d1d8b59afbde921490b1a9d009cf8090c822e20f
thai-bug <hxt12021999@gmail.com>
2022-06-19 11:18:37 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: [],
   theme: {
-    extend: {
-      colors:{
-        blue: 'blue',
-        'primary': '#f1f1f1'
-      },
-      height:{
-        '4': '4rem',
-      },
-      width:{
-        '18': '18rem',
-        '5.5': '5.5rem',
-      },
-      transitionProperty:{
-        width: 'width'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
